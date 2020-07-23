4c4e0fe2014715dcbf090d16548071d61f8cb516
Gunisetty Ramesh Babu <ramesh.babu.gunisetty@volvo.com>
2020-07-23 12:33:48 +0530

    First Files

---
+++ src/app/app.config.ts
@@ -0,0 +1,16 @@
+const { CLIENT_ID, ISSUER, OKTA_TESTING_DISABLEHTTPSCHECK } = process.env;
+
+export default {
+  oidc: {
+    clientId: `0oam7vpo3tjBa5UV74x6`,
+    issuer: `https://azdesmfb.okta.com/oauth2/default`,
+    redirectUri: 'http://localhost:8081/implicit/callback',
+    scopes: ['openid', 'profile', 'email'],
+    testing: {
+      disableHttpsCheck: true
+    }
+  },
+  resourceServer: {
+    messagesUrl: 'http://localhost:8000/api/messages',
+  },
+};
